8fe04de0c93206888261364c43772dbf1594abaf
SELVAPRASANTH-SELLASAMY <prasanthsamy61@gmail.com>
2023-11-06 11:37:23 +0530

    gonna finish

--- index.js
+++ index.js
@@ -1,40 +1,52 @@
-let values = 0;
-let a = null;
-var operation;
-window.onload = function(){
-    const btn = document.querySelectorAll('.num');
-    for(let i=0;i<btn.length;i++){
-        btn[i].addEventListener('click',()=>{
-            // values = btn[i].value;
-            values = (values*10)+Number(btn[i].value);
-        })
-    }
-    const arm = document.querySelectorAll('.arithmaticKeys');
-    for(let i=0;i<arm.length;i++){
-        arm[i].addEventListener('click',()=>{
-            operation = arm[i].value;
-            if(a !== null){
-                if(operation === '+'){
-                    a = a+values;
-                }
-                else if(operation === '-'){
-                    a = a-values;
-                }
-                else if(operation === '*'){
-                    a = a*values;
-                }
-                else if(operation === '/'){
-                    a = a/values;
-                }
-            }
-            else{
-                a = values;
-            }
-            values = 0;
-        })
-    }
+var a;
+var b = 0;
+var operator;
+var prevOperator;
+const numKey = document.querySelectorAll('.num');
+for(let i = 0;i < numKey.length;i++){
+    numKey[i].addEventListener('click', () => {
+        b = ((b*10)+Number(numKey[i].value));
+        document.querySelector('.capturing-texts').innerText = b;
+    })
 }
-const print = () =>{
-    alert(values+a)
-    alert(a)
+const ArithmeticOperation = {
+    '+' : function(x,y){return x+y},
+    '-' : function(x,y){return x-y},
+    '*' : function(x,y){return x*y},
+    '/' : function(x,y){return x/y}
+}
+const ArithmeticKeys = document.querySelectorAll('.arithmaticKeys');
+for(let i = 0;i < ArithmeticKeys.length;i++){
+    ArithmeticKeys[i].addEventListener('click', () => {
+        if(operator === undefined){
+            operator = ArithmeticKeys[i].value;
+        }
+        else{
+            prevOperator = operator;
+            operator = ArithmeticKeys[i].value;
+        }
+        document.querySelector('.operation').innerText = operator;
+        if(a === undefined){
+            a = b;
+        }
+        else{
+            a = ArithmeticOperation[prevOperator](a,b);
+        }
+        document.querySelector('.captured-texts').innerText = a;
+        b = 0;
+    })
+}
+document.getElementById('equal').addEventListener('click', () => {
+    check();
+})
+document.getElementById('delete').addEventListener('click', () => {
+    b = Math.trunc(b/10);
+    document.querySelector('.capturing-texts').innerText = b;
+})
+function check(){
+    a = ArithmeticOperation[operator](a,b)
+    operator = undefined
+    b = 0
+    document.querySelector('.capturing-texts').innerText = b;
+    document.querySelector('.captured-texts').innerText = a;
 }
