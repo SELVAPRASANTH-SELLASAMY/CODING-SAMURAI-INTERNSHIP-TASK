b099e3ecc331261066828dc8ae5dfc884cfc6b83
SELVAPRASANTH-SELLASAMY <prasanthsamy61@gmail.com>
2023-11-07 22:07:19 +0530

    reconfigured backend fuctionalities

--- index.js
+++ index.js
@@ -1,79 +1,118 @@
-var activeTheme = localStorage.getItem('Theme')
+var a
+var b
+var c = 0
+var operation
+var prevOperation
+var activeTheme = localStorage.getItem('Theme');
 if(activeTheme === 'dark'){
     dark();
 }
-var a;
-var b = 0;
-var operator;
-var prevOperator;
-const numKey = document.querySelectorAll('.num');
-for(let i = 0;i < numKey.length;i++){
-    numKey[i].addEventListener('click', () => {
-        b = ((b*10)+Number(numKey[i].value));
-        document.querySelector('.capturing-texts').innerText = b;
+var isClickedArithmeticKey = false
+const capTextA = document.querySelector('.captured-texts-area > .text-a');
+const capTextB = document.querySelector('.captured-texts-area > .text-b');
+const operationSymbol = document.querySelector('.captured-texts-area > .operation');
+const typing = document.querySelector('.capturing-text-area > .capturing-text');
+const numKeys  = document.getElementsByClassName('num');
+for(let i = 0;i < numKeys.length;i++){
+    numKeys[i].addEventListener('click', () => {
+        c = ((c*10)+Number(numKeys[i].value));
+        typing.innerText = c;
+        // console.log(c)
     })
 }
-const ArithmeticOperation = {
+
+const compute = {
     '+' : function(x,y){return x+y},
     '-' : function(x,y){return x-y},
     '*' : function(x,y){return x*y},
     '/' : function(x,y){return x/y}
 }
-const ArithmeticKeys = document.querySelectorAll('.arithmaticKeys');
+
+const ArithmeticKeys = document.getElementsByClassName('arithmaticKeys');
 for(let i = 0;i < ArithmeticKeys.length;i++){
     ArithmeticKeys[i].addEventListener('click', () => {
-        if(operator === undefined){
-            operator = ArithmeticKeys[i].value;
+        if(operation !== undefined){
+            prevOperation = operation
         }
-        else{
-            prevOperator = operator;
-            operator = ArithmeticKeys[i].value;
+        operation = ArithmeticKeys[i].value;
+        isClickedArithmeticKey = true
+        if(isClickedArithmeticKey){
+            if(a !== undefined){
+                a = compute[prevOperation](a,c);
+                c = 0
+                capTextA.innerText = a;
+                typing.innerText = '';
+                capTextB.innerText = '';
+            }
+            else {
+                a = c;
+                c = 0;
+                capTextA.innerText = a;
+                typing.innerText = '';
+            }
         }
-        document.querySelector('.operation').innerText = operator;
-        if(a === undefined){
-            a = b;
-        }
-        else{
-            a = ArithmeticOperation[prevOperator](a,b);
-        }
-        document.querySelector('.captured-texts').innerText = a;
-        b = 0;
+        operationSymbol.innerText = ArithmeticKeys[i].value;
+        // console.log(ArithmeticKeys[i].value)
     })
 }
-document.getElementById('equal').addEventListener('click', () => {
-    check();
-})
-document.getElementById('delete').addEventListener('click', () => {
-    if(b>0){
-        b = Math.trunc(b/10);
-        document.querySelector('.capturing-texts').innerText = b;
-    }
-    else if(b<=0){
-        if(a>0){
-            a = Math.trunc(a/10);
-            document.querySelector('.captured-texts').innerText = a;
-        }
-    }
-})
-function check(){
-    a = ArithmeticOperation[operator](a,b)
-    operator = undefined
-    b = 0
-    document.querySelector('.capturing-texts').innerText = b;
-    document.querySelector('.captured-texts').innerText = a;
-}
 
-function setTheme(){
-    if(activeTheme === 'dark')//activeTheme set in localStorage
-    {
-        bright();
-        window.location.href='/'
+const equalBtn = document.getElementById('equal');
+equalBtn.addEventListener('click', () => {
+    if(a !== undefined && c !== undefined){
+        b = c;
+        capTextB.innerText = b;
+        c = compute[operation](a,c);
+        typing.innerText = c;
     }
-    else {
-        dark();
-        window.location.href='/'
+})
+
+const deleteBtn = document.getElementById('delete');
+deleteBtn.addEventListener('click', () => {
+    if(c === 0){
+        typing.innerText = '';
     }
-}
+    if(b === 0){
+        capTextB.innerText = '';
+        operationSymbol.innerText = '';
+    }
+    if(a === 0){
+        capTextA.innerText = '';
+    }
+    if(c>0){
+        c = Math.trunc(c/10);
+        typing.innerText = c;
+    }
+    else if(b>0){
+        b = Math.trunc(b/10);
+        capTextB.innerText = b;
+    }
+    
+    else if(a>0){
+        a = Math.trunc(a/10);
+        capTextA.innerText = a;
+    }
+})
+
+const clearAll = document.querySelector('.clearAll-btn');
+clearAll.addEventListener('click', () => {
+    a = undefined
+    b = undefined
+    c = 0
+    capTextA.innerText  = '';
+    capTextB.innerText = '';
+    typing.innerText = '';
+    operationSymbol.innerText = '';
+    operation = undefined
+    prevOperation = undefined
+})
+
+const themeBtn = document.querySelector('.theme');
+themeBtn.addEventListener('click', () => {
+    if(activeTheme === 'dark'){
+        return bright();
+    }
+    return dark();
+})
 
 function dark(){
     document.querySelector('body').style.backgroundColor = 'black';
@@ -85,7 +124,6 @@ function dark(){
 function bright(){
     document.querySelector('body').style.backgroundColor = '';
     document.querySelector('.container').style.backgroundColor = '';
-    document.querySelector('.container').style.border = '';
     document.querySelector('.text > h1').style.color = '';
     document.querySelector('.theme').innerHTML = '<img src="/assets/darkmode.svg" alt="theme">';
     localStorage.removeItem('Theme')
